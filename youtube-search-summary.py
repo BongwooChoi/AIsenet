b7899be2d15da044cac0d6f3916a33fb3c42c7dc
BongwooChoi <47071650+BongwooChoi@users.noreply.github.com>
2024-08-18 07:18:28 +0900

    Update youtube-search-summary.py

--- youtube-search-summary.py
+++ youtube-search-summary.py
@@ -8,6 +8,7 @@ import requests
 import urllib.parse
 import pandas as pd
 import plotly.graph_objects as go
+import yfinance as yf
 
 # Streamlit 앱 설정
 st.set_page_config(page_title="AI 금융정보 검색 및 분석 서비스", page_icon="📈", layout="wide")
